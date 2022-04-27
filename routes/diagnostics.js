04f34f44a1f123196964de1188465498856ffc2b
miacarmen <maitreyacarmen@gmail.com>
2022-04-27 13:13:19 -0700

    did some work that doesn't work

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -5,11 +5,16 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile('./db/diagnostics.json').then((data) => 
+    res.json(JSON.parse(data)));
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  console.log(req.body);
+
+  
 });
 
 module.exports = diagnostics;
